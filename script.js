71647e79321325fcf4db319cdd96a5676547fc29
edivaldo.costa <edivaldo.costa@localiza.com>
2024-11-20 22:41:04 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const btnGenerate = document.querySelector("#generate-pdf");
+const btnIdioma = document.querySelector("#idioma");
 
 btnGenerate.addEventListener("click", () => {
     // Conteúdo do PDF
     const content = document.querySelector("#content");
 
-    // Ocultar o botão temporariamente
+    // Ocultar os botões temporariamente
     btnGenerate.style.display = "none";
+    btnIdioma.style.display = "none";
 
     // Configuração do formato final do PDF
     const options = {
         margin: [10, 10, 10, 10],
-        filename: "currículo-edivaldo.pdf",
+        filename: "currículo-edivaldo-junior.pdf",
         html2canvas: { scale: 2 },
         jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
     };
@@ -21,13 +23,15 @@ btnGenerate.addEventListener("click", () => {
         .from(content)
         .save()
         .then(() => {
-            // Reexibir o botão após o PDF ser gerado
+            // Reexibir os botões após o PDF ser gerado
             btnGenerate.style.display = "block";
+            btnIdioma.style.display = "block";
         })
         .catch((err) => {
             console.error("Erro ao gerar o PDF:", err);
-            // Reexibir o botão em caso de erro
+            // Reexibir os botões em caso de erro
             btnGenerate.style.display = "block";
+            btnIdioma.style.display = "block";
         });
 });
 
