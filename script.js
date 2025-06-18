bbb29826de26472f11763dd7819beed36ab9ec00
EdivaldoCostaLimaJr <edivaldocostadv@gmail.com>
2025-06-18 11:24:13 -0300

    adiciona o dark-mode

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
 const btnGenerate = document.querySelector("#generate-pdf");
 const btnIdioma = document.querySelector("#idioma");
+const btnDarkMode = document.querySelector("#dark-mode-toggle");
 
+// Função para gerar PDF
 btnGenerate.addEventListener("click", () => {
-    // Conteúdo do PDF
     const content = document.querySelector("#content");
 
+    // Esconde botões
     btnGenerate.style.display = "none";
     btnIdioma.style.display = "none";
+    btnDarkMode.style.display = "none";
 
-    // Configuração do formato final do PDF
+    // Verificar se está no modo dark
+    const darkModeAtivo = document.body.classList.contains('dark-mode');
+    if (darkModeAtivo) {
+        document.body.classList.remove('dark-mode');
+    }
+
     const options = {
-        margin: [10, 10, 10, 10],
-        filename: "currículo-edivaldo-junior.pdf",
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+        margin: 5,
+        filename: 'curriculo-edivaldo-junior.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 3, useCORS: true },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
     };
 
-    // Gerar e baixar o PDF
+
     html2pdf()
         .set(options)
         .from(content)
         .save()
         .then(() => {
-            // Reexibir os botões após o PDF ser gerado
+            // Restaurar dark-mode se estava ativo
+            if (darkModeAtivo) {
+                document.body.classList.add('dark-mode');
+            }
+
+            // Mostrar botões novamente
             btnGenerate.style.display = "block";
             btnIdioma.style.display = "block";
+            btnDarkMode.style.display = "block";
         })
         .catch((err) => {
             console.error("Erro ao gerar o PDF:", err);
-            // Reexibir os botões em caso de erro
+
+            // Restaurar dark-mode se estava ativo
+            if (darkModeAtivo) {
+                document.body.classList.add('dark-mode');
+            }
+
+            // Mostrar botões novamente
             btnGenerate.style.display = "block";
             btnIdioma.style.display = "block";
+            btnDarkMode.style.display = "block";
         });
 });
 
+// Função para alternar Dark Mode
+btnDarkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
